e3ee7708529e610b565158a326b414203069e4f4
Manueljospino <manueljospino@unicesar.edu.co>
2026-04-23 06:05:15 -0500

    Create PersonRepository.java

---
+++ Proyect/src/main/java/com/universidad/asistencia/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.universidad.asistencia;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import  org.springframework.data.jpa.repository.JpaRepository;
+import  org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByMail(String mail);
+}
